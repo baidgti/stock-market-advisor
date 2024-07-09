5457b94f8a83e726f9860c46ab99003ba944e7b8
baidgti1 <baidgti1@hotmail.com>
2024-07-09 14:37:34 +0200

    Initial commit

---
+++ src/main/java/be03/lukacsrichard/App.java
@@ -0,0 +1,13 @@
+package be03.lukacsrichard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
